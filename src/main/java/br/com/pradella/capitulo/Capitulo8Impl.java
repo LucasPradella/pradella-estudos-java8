bc90b100a944a3074aea7838d85a1039d6bbd1bf
Lucas <lucas.pradella@hotmail.com>
2018-02-05 15:28:49 -0200

    Adicionando java.nio.files

--- src/main/java/br/com/pradella/capitulo/Capitulo8Impl.java
+++ src/main/java/br/com/pradella/capitulo/Capitulo8Impl.java
@@ -2,6 +2,12 @@ package br.com.pradella.capitulo;
 
 import static java.util.Arrays.asList;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
@@ -9,12 +15,14 @@ import java.util.Random;
 import java.util.function.IntBinaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
+import br.com.pradella.entidade.Grupo;
 import br.com.pradella.entidade.Usuario;
 
 public class Capitulo8Impl {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		Usuario user1 = new Usuario("Hanah", 120);
 		Usuario user2 = new Usuario("Arya", 150);
@@ -128,8 +136,62 @@ public class Capitulo8Impl {
 
 		
 		
-		
-	}
+		// flatmap e java.nio.files
+
+				Files.list(Paths.get("./src/main/resources/java8"))
+					.forEach(System.out::println);
+
+				Files.list(Paths.get("./src/main/resources/java8"))
+					.filter(p -> p.toString().endsWith(".java"))
+					.forEach(System.out::println);
+
+
+				Files.list(Paths.get("./src/main/resources/java8"))
+							.filter(p -> p.toString().endsWith(".java"))
+							.map(p -> lines(p))
+							.forEach(System.out::println);
+
+				Stream<Stream<String>> strings = 
+					Files.list(Paths.get("./src/main/resources/java8"))
+						.filter(p -> p.toString().endsWith(".java"))
+						.map(p -> lines(p));
+
+				Files.list(Paths.get("./src/main/resources/java8"))
+					.filter(p -> p.toString().endsWith(".java"))
+					.flatMap(p -> lines(p))
+					.forEach(System.out::println);
+
+				
+				IntStream chars =
+					Files.list(Paths.get("./src/main/resources/java8"))
+						.filter(p -> p.toString().endsWith(".java"))
+						.flatMap(p -> lines(p))
+						.flatMapToInt((String s) -> s.chars());
+
+
+				Grupo englishSpeakers = new Grupo();
+				englishSpeakers.add(user1);
+				englishSpeakers.add(user2);
+
+				Grupo spanishSpeakers = new Grupo();
+				spanishSpeakers.add(user2);
+				spanishSpeakers.add(user3);
+
+				List<Grupo> groups = Arrays.asList(englishSpeakers, spanishSpeakers);
+				groups.stream()
+					.flatMap(g -> g.getUsuarios().stream())
+					.distinct()
+					.forEach(System.out::println);
+
+			}
+
+			static Stream<String> lines(Path p) {
+				try {
+					return Files.lines(p);
+				} catch(IOException e) {
+					throw new UncheckedIOException(e);
+				}
+			}
 	
 	
 
